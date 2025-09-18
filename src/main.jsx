f232a06b7be468287413090a096aa3eda00c9ce5
youtubejd56 <kkvinayak718@gmail.com>
2025-09-18 17:10:53 +0530

    vite-config-update

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App"
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/school-management-system">
     <App />
   </BrowserRouter>
 );
