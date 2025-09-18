b744f81c78fa88c781472288c07f391bee4088b4
youtubejd56 <kkvinayak718@gmail.com>
2025-09-18 14:52:12 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/school-management-system/', // ✅ no spaces
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
@@ -14,14 +16,16 @@ export default defineConfig({
         short_name: 'MyApp',
         description: 'A PWA built with Vite + React + Django backend',
         theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
         icons: [
           {
-            src: './src/asset/mobile.png',
+            src: '/mobile.png',  // must be in public/
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: './src/asset/Desktop.png',
+            src: '/desktop.png', // must be in public/
             sizes: '512x512',
             type: 'image/png'
           }
@@ -33,7 +37,5 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:8000'
     }
-  },
-  
-  plugins: [react(), tailwindcss()],
+  }
 })
