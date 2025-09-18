f8911c4b50445a433c6a29bc84ffa420d8717340
youtubejd56 <kkvinayak718@gmail.com>
2025-09-18 13:28:26 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Django React App',
+        short_name: 'MyApp',
+        description: 'A PWA built with Vite + React + Django backend',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: './src/asset/mobile.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './src/asset/Desktop.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': 'http://localhost:8000'
